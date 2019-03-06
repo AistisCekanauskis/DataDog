08e1d7a376cd18b400e601c90c45fdb72c828102
AistisCekanauskis <aistis.cekanauskiss@gmail.com>
2019-03-06 11:37:43 +0200

    Update AppBundle.php

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -1,9 +1,20 @@
 <?php
-
-namespace AppBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AppBundle extends Bundle
-{
+.....
+class ConfirmEmailController extends Controller {
+  ....   
+  public function confirmEmailAction($id, $hash) {
+    $form = $this->applicationRepository->findByHash($id, $hash);
+    
+    // Current place - start
+    $this->workflow->apply($form, 'confirm_email');
+    $this->workflow->apply($form, 'validate_application');
+    
+    try {
+      $this->workflow->apply($form, 'confirm_email');
+    } catch(LogicException $e) {
+      // Cannot apply transition, probably already confirmed
+    }
+    ....
+  }
+  
 }
